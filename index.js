67d00119a902db00bfadae1930606fba151b76ee
Frank Purcell <fxpurcell@gmail.com>
2017-03-20 16:55:47 -0700

    minor changes ... for windows ... doesn't work

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ const loader = require('./lib/loader');
 // step 1: make sure we have valid transit configuration
 var peliasConfig = require('pelias-config').generate(true);
 var transitConfig = _.get(peliasConfig, 'imports.transit');
-const {error, value} = Joi.validate(transitConfig, schema);
-if(error || transitConfig == undefined) {
-    utils.logger.error("transit config error: " + error);
+const error = Joi.validate(transitConfig, schema)[0];
+
+// step 2: config error checking ... note potential early exit
+if(error) {
+    utils.logger.error(`transit config error: ${error}`);
+    process.exit(0);
+}
+if(transitConfig == undefined || transitConfig.files == undefined) {
+    utils.logger.error(`your pelias.json config lacks a transit.files array`);
     process.exit(0);
 }
 
-// step 2: we've got a good transit config, so let's start loading Pelias...
+// step 3: we've got a good transit config, so let's start loading Pelias...
 utils.startTiming();
 
-// step 3: load each of the configured transit datasets
+// step 4: load each of the configured transit datasets
 loader.setTransitConfig(transitConfig);
 transitConfig.files.forEach(loader.load);
