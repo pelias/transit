80af0b2fd36cdeda65c9a1156267b85450b7449e
Frank Purcell <fxpurcell@gmail.com>
2017-03-28 21:55:35 -0700

    rename to feeds in config

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const loader = require('./lib/loader');
 
 
 // step 1: make sure we have valid transit configuration
-var peliasConfig = require('pelias-config').generate(true);
-var transitConfig = _.get(peliasConfig, 'imports.transit');
-const error = Joi.validate(transitConfig, schema)[0];
+const peliasConfig = require('pelias-config').generate(true);
+const transitConfig = _.get(peliasConfig, 'imports.transit');
+const valid = Joi.validate(transitConfig, schema);
+const error = valid[0];
 
 // step 2: config error checking ... note potential early exit
 if(error) {
     utils.logger.error(`transit config error: ${error}`);
     process.exit(0);
 }
-if(transitConfig == undefined || transitConfig.files == undefined) {
-    utils.logger.error(`your pelias.json config lacks a transit.files array`);
+if(transitConfig == undefined || transitConfig.feeds == undefined) {
+    utils.logger.error(`your pelias.json config lacks a transit.feeds array`);
     process.exit(0);
 }
 
--- schema.js
+++ schema.js
@@ -4,18 +4,15 @@ const Joi = require('joi');
 
 const arrayObj = Joi.object().keys({
     filename: Joi.string().required(),
-    type: Joi.string().required().valid('stops', 'pr', 'tc', 'tvm', 'intersection'),
+    type: Joi.string().required().valid('stops', 'pr', 'tc', 'tvm', 'intersection', 'carshare', 'bikeshare'),
     agencyId: Joi.string().required(),
 }).requiredKeys('filename', 'agencyId', 'type');
 
 // Schema Configuration
 // datapath: string (required)
-// files: array of strings
-// deduplicate: boolean
-// adminLookup: boolean
+// feeds: array of objects ... see above (required)
 module.exports = Joi.object().keys({
     datapath: Joi.string(),
-    adminLookup: Joi.boolean(),
-    files: Joi.array().min(1).items(arrayObj),
-}).requiredKeys('datapath', 'files').unknown(true);
+    feeds: Joi.array().min(1).items(arrayObj),
+}).requiredKeys('datapath', 'feeds');
 
