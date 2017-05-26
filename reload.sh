557deb22061d588db71f96015deec2d14389c956
Frank Purcell <fxpurcell@gmail.com>
2017-05-26 15:50:18 -0700

    docs / echos

--- reload.sh
+++ reload.sh
@@ -1,15 +1,18 @@
 HOME_DIR=/srv/pelias_loader
 DATA_DIR=$HOME_DIR/data
 PROJ_DIR=$HOME_DIR/projects
+OSM_FILE=or-wa.pbf
+POLYLINES_FILE=or-wa.polylines
 
 MAPS_SVR=http://maps7/pelias
 
-# step 1: download transit, OSM and OR-WA data
+# step 1: download and prep transit, OSM, OA and WOF datasets
 cd $DATA_DIR
 rm -rf old
 mkdir old
 mv oa osm transit wof ./old/
 
+echo "DOWNLOAD transit data from $MAPS_SVR"
 mkdir $DATA_DIR/transit
 cd $DATA_DIR/transit
 for i in CHERRIOTS.zip C-TRAN.zip RIDECONNECTION.zip SAM.zip SWAN.zip SMART.zip TRIMET.zip intersections.csv TRIMET-landmarks.csv
@@ -17,18 +20,21 @@ do
   wget $MAPS_SVR/transit/$i .
 done
 
+echo "DOWNLOAD $OSM_FILE data from $MAPS_SVR"
 mkdir $DATA_DIR/osm
 cd $DATA_DIR/osm
-wget $MAPS_SVR/osm/or-wa.pbf
+wget $MAPS_SVR/osm/$OSM_FILE
 
+echo "CREATE $POLYLINES_FILE data from $OSM_FILE data from $MAPS_SVR"
 cd $DATA_DIR/osm
-$PROJ_DIR/go_polylines/bin/pbf streets or-wa.pbf > or-wa.polylines
+$PROJ_DIR/go_polylines/bin/pbf streets $OSM_FILE > $POLYLINES_FILE
 
 ##
 ## gate script -- just download updated transit (commands above) if a cmd line param included
 ##
 if [ ${#} -eq 0 ];then
-
+    # step 1a: grab OpenAddress data for TriMet area...
+    echo "DOWNLOAD OpenAddress data"
     mkdir $DATA_DIR/oa
     cd $DATA_DIR/oa
     mv openaddr-collected-us_west.zip openaddr-collected-us_west.zip-OLD
@@ -37,31 +43,35 @@ if [ ${#} -eq 0 ];then
     unzip openaddr-collected-us_west.zip
 
     # step 1b: download wof admin data
+    echo "DOWNLOAD whosonfirst data"
     cd $PROJ_DIR/whosonfirst
     npm install
     npm run download -- --admin-only
-
 else
-
-    # alternate to the gate ... move oa and wof datasets back into position
+    # step 1ab alternate: move OLD oa and wof datasets back into position and move on
     mv $DATA_DIR/old/oa  $DATA_DIR/
     mv $DATA_DIR/old/wof $DATA_DIR/
-
 fi
 
 # step 1c. get pelias.transit's data ready
+echo "PREP Transit Data"
 cd $PROJ_DIR/pelias.transit.loader/
+git pull
 npm install
 npm run prep_data
 
+
 # step 2: create new / empty index
+echo "PREP ElasticSearch SCHEMA"
 cd $PROJ_DIR/schema
 curl -XDELETE 'localhost:9200/pelias?pretty'
 npm install
 node scripts/create_index.js
 
+
 # step 3: load the system...
 loaders=(pelias.transit.loader polylines openaddresses openstreetmap)
+echo "LOADING the ${loaders[*]} datasets into ElasticSearch"
 for l in "${loaders[@]}"
 do
     echo $l
@@ -70,8 +80,10 @@ do
     npm start
 done
 
+
 # step 4: create street.db and address.db for interpolation (*again, gate script)
 # see: https://github.com/pelias/interpolation#building-the-databases
+echo "BUILDING street.db and address.db for interpolation"
 cd $PROJ_DIR/interpolation/
 rm *.log *.db osm_data.json
 
@@ -81,12 +93,12 @@ export TIGER_LOG=$DATA_DIR/tiger/download.log
 flock -n $TIGER_LOG -c './script/update_tiger.sh > $TIGER_LOG 2>&1' &
 
 # 4b: POLYLINE into street.db
-node cmd/polyline street.db < /data/osm/or-wa.polylines
+node cmd/polyline street.db < /data/osm/$POLYLINES_FILE
 
 # 4c: OA into STREET_DB & ADDRESS_DB
 for x in clark city_of_richland
 do
-    OA_CSV="$DATA_DIR/oa/us/wa/$x.csv"
+    OA_CSV="$DATA_DIR/oa/us/wa/$x.csv"pp
     echo $OA_CSV
     node cmd/oa address.db street.db < $OA_CSV > openaddess-wa.log 2>&1
 done
@@ -98,7 +110,7 @@ do
 done
 
 # 4d: OSM into STREET_DB & ADDRESS_DB
-pbf2json -tags="addr:housenumber+addr:street" $DATA_DIR/osm/or-wa.pbf > osm_data.json
+pbf2json -tags="addr:housenumber+addr:street" $DATA_DIR/osm/$OSM_FILE > osm_data.json
 node cmd/osm address.db street.db < osm_data.json
 
 # 4e: OSM into STREET_DB & ADDRESS_DB
